478de3cf347c46f64b1d3cac18aafbdc0dec31f7
Abdelrahman Mohamed <obada.semary@gmail.com>
2023-07-09 01:10:37 +0300

    Add Station Model

--- VoltlinesBusStations/Models/Station.swift
+++ VoltlinesBusStations/Models/Station.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+struct Station: Codable {
+    let centerCoordinates: String?
+    let id: Int?
+    let name: String?
+    let trips: [Trip]?
+    let tripsCount: Int?
+
+    enum CodingKeys: String, CodingKey {
+        case centerCoordinates = "center_coordinates"
+        case id, name, trips
+        case tripsCount = "trips_count"
+    }
+}
