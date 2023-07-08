5838534c5ec3bf9e7729e0ff7767ba6f0bb66176
Abdelrahman Mohamed <obada.semary@gmail.com>
2023-07-09 01:10:47 +0300

    Add Trip Model

--- VoltlinesBusStations/Models/Trip.swift
+++ VoltlinesBusStations/Models/Trip.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+
+struct Trip: Codable {
+    let busName: String?
+    let id: Int?
+    let time: String?
+
+    enum CodingKeys: String, CodingKey {
+        case busName = "bus_name"
+        case id, time
+    }
+}
