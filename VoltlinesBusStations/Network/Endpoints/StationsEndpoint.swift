61bcd38db0caedecfb577a4d162f1c0a68516a56
Abdelrahman Mohamed <obada.semary@gmail.com>
2023-07-09 01:08:08 +0300

    Add StationsEndpoint

--- VoltlinesBusStations/Network/Endpoints/StationsEndpoint.swift
+++ VoltlinesBusStations/Network/Endpoints/StationsEndpoint.swift
@@ -6,3 +6,40 @@
 //
 
 import Foundation
+
+enum StationsEndpoint {
+    case stations
+}
+
+extension StationsEndpoint: Endpoint {
+
+    var path: String {
+        switch self {
+        case .stations:
+            return "/case-study/6/stations"
+        }
+    }
+
+    var method: RequestMethod {
+        switch self {
+        case .stations:
+            return .get
+        }
+    }
+
+    var header: [String: String]? {
+        switch self {
+        case .stations:
+            return [
+                "Content-Type": "application/json;charset=utf-8"
+            ]
+        }
+    }
+
+    var body: [String: String]? {
+        switch self {
+        case .stations:
+            return nil
+        }
+    }
+}
