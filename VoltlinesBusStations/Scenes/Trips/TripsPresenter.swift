8b3f03203267dce223b577ad4e39fc336a0ea542
Abdelrahman Mohamed <obada.semary@gmail.com>
2023-07-11 02:30:38 +0300

    introduce TripsPresenter

--- VoltlinesBusStations/Scenes/Trips/TripsPresenter.swift
+++ VoltlinesBusStations/Scenes/Trips/TripsPresenter.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+
+protocol TripsPresenterDelegate: AnyObject {
+    var presenter: TripsPresenter? { get set }
+    func presentStations(station: Station)
+}
+
+class TripsPresenter {
+
+    private weak var delegate: TripsPresenterDelegate?
+    var station: Station?
+
+    init(delegate: TripsPresenterDelegate, station: Station) {
+        self.delegate = delegate
+        self.station = station
+    }
+
+    func viewDidLoad() {
+        guard let newStation = station else { return }
+        delegate?.presentStations(station: newStation)
+    }
+}
