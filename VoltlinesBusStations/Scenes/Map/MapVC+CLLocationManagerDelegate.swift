a7e230455b362af8fd300af909aafab800e6b094
Abdelrahman Mohamed <obada.semary@gmail.com>
2023-07-10 03:32:12 +0300

    implement CLLocationManagerDelegate

--- VoltlinesBusStations/Scenes/Map/MapVC+CLLocationManagerDelegate.swift
+++ VoltlinesBusStations/Scenes/Map/MapVC+CLLocationManagerDelegate.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+import MapKit
+
+extension MapViewController: CLLocationManagerDelegate {
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let locValue: CLLocationCoordinate2D = manager.location?.coordinate else { return }
+        let span = MKCoordinateSpan(latitudeDelta: 0.02, longitudeDelta: 0.02)
+
+        let coordinates = CLLocationCoordinate2D(latitude: locValue.latitude, longitude: locValue.longitude)
+
+        let region = MKCoordinateRegion(center: coordinates, span: span)
+        mapView.setRegion(region, animated: true)
+    }
+
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        checkLocationAuthorization()
+    }
+
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+    }
+}
