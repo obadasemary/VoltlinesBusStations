b266455eca814d7254191983250ee08c6e7869f9
Abdelrahman Mohamed <obada.semary@gmail.com>
2023-07-10 03:32:43 +0300

    implement MapPresenterDelegate

--- VoltlinesBusStations/Scenes/Map/MapVC+MapPresenterDelegate.swift
+++ VoltlinesBusStations/Scenes/Map/MapVC+MapPresenterDelegate.swift
@@ -6,3 +6,36 @@
 //
 
 import Foundation
+
+extension MapViewController: MapPresenterDelegate {
+
+    func showIndicator() {
+        activityIndicator.startAnimating()
+    }
+
+    func hideIndicator() {
+        DispatchQueue.main.async {
+            self.activityIndicator.stopAnimating()
+            self.activityIndicator.hidesWhenStopped = true
+        }
+    }
+
+    func showError(error: String) {
+        print("Error:", error)
+    }
+
+    func presentStations(stations: [Station]) {
+        var newStations = stations
+        newStations.reverse()
+        DispatchQueue.main.async {
+            newStations.forEach { station in
+                self.setupMap(
+                    name: station.name ?? "",
+                    lat: station.lat,
+                    long: station.long,
+                    tripCount: station.tripsCount ?? 0
+                )
+            }
+        }
+    }
+}
