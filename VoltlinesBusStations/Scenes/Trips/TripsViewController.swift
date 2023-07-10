dbd5b2c7ee5ad0288ddf3e401d832f8fd1bc8171
Abdelrahman Mohamed <obada.semary@gmail.com>
2023-07-10 08:28:21 +0300

    Add trips VC

---
+++ VoltlinesBusStations/Scenes/Trips/TripsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  TripsViewController.swift
+//  VoltlinesBusStations
+//
+//  Created by Abdelrahman Mohamed on 10.07.2023.
+//
+
+import UIKit
+
+class TripsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+}
