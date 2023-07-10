62a88843c97c1237764c2bca4ce3f070788d5ef9
Abdelrahman Mohamed <obada.semary@gmail.com>
2023-07-10 03:32:32 +0300

    implement MKMapViewDelegate

--- VoltlinesBusStations/Scenes/Map/MapVC+MKMapViewDelegate.swift
+++ VoltlinesBusStations/Scenes/Map/MapVC+MKMapViewDelegate.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+import MapKit
+
+extension MapViewController: MKMapViewDelegate {
+
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+
+        let identifier = "pin"
+
+        if annotation is MKUserLocation {
+            return nil
+        }
+
+        annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier)
+
+        if annotationView == nil {
+            annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+            annotationView.canShowCallout = true
+            annotationView.image = UIImage(named: "Point")
+        } else {
+            annotationView.annotation =  annotation
+        }
+
+        return annotationView
+    }
+}
