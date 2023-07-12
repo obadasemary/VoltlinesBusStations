dbcba2dc28d32ec1c189b683c347d5d54861b87a
Abdelrahman Mohamed <obada.semary@gmail.com>
2023-07-12 03:29:48 +0300

    implement TripsViewController

--- VoltlinesBusStations/Scenes/Trips/TripsViewController.swift
+++ VoltlinesBusStations/Scenes/Trips/TripsViewController.swift
@@ -7,21 +7,54 @@
 
 import UIKit
 
-class TripsViewController: UIViewController, TripsPresenterDelegate {
+class TripsViewController: UIViewController {
 
-    func showError(error: String) {
-        print("TripsViewController.Error:", error)
-    }
-
-    func presentStations(station: Station) {
-        print("TripsViewController.station:", station)
-    }
+    @IBOutlet weak var tableView: UITableView!
     
     var presenter: TripsPresenter?
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        title = "Trips"
+        setupTableView()
+
         presenter?.viewDidLoad()
+    }
+}
+
+extension TripsViewController: TripsPresenterDelegate {
+
+    func showError(error: String) {
+        print("TripsViewController.Error:", error)
+    }
+
+    func presentStations(station: Station) {
+        tableView.reloadData()
+        print("TripsViewController.station:", station)
+    }
+}
+
+extension TripsViewController: UITableViewDataSource {
+
+    func setupTableView() {
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.dataSource = self
+        tableView.register(
+            UINib(nibName: TripsTableViewCell.identifier, bundle: nil),
+            forCellReuseIdentifier: TripsTableViewCell.identifier
+        )
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return presenter?.getTripsCount() ?? 0
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        let cell = tableView.dequeueReusableCell(withIdentifier: TripsTableViewCell.identifier, for: indexPath) as! TripsTableViewCell
+        presenter?.configure(cell: cell, for: indexPath.row)
+        presenter?.didTap(cell: cell, for: indexPath.row)
+        return cell
     }
 }
