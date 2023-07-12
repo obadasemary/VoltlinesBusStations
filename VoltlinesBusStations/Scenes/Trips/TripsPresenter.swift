6bfc2a07c68eae2a2056419306b7515f60bbb51f
Abdelrahman Mohamed <obada.semary@gmail.com>
2023-07-12 03:30:05 +0300

    implement TripsPresenter

--- VoltlinesBusStations/Scenes/Trips/TripsPresenter.swift
+++ VoltlinesBusStations/Scenes/Trips/TripsPresenter.swift
@@ -6,10 +6,16 @@
 //
 
 import Foundation
+import UIKit
 
 protocol TripsPresenterDelegate: AnyObject {
     var presenter: TripsPresenter? { get set }
     func presentStations(station: Station)
+}
+
+protocol TripsCellDelegate {
+    func displayBusName(value: String)
+    func displayBusTime(value: String)
 }
 
 class TripsPresenter {
@@ -25,5 +31,22 @@ class TripsPresenter {
     func viewDidLoad() {
         guard let newStation = station else { return }
         delegate?.presentStations(station: newStation)
+    }
+
+    func getTripsCount() -> Int {
+        return station?.tripsCount ?? 0
+    }
+
+    func configure(cell: TripsCellDelegate, for index: Int) {
+        let trip = station?.trips?[index]
+        cell.displayBusName(value: trip?.busName ?? "")
+        cell.displayBusTime(value: trip?.time ?? "")
+    }
+
+    func didTap(cell: TripsTableViewCell, for index: Int) {
+        cell.didBookTrip = { [unowned self] in
+            let trip = station?.trips?[index]
+            print(trip)
+        }
     }
 }
