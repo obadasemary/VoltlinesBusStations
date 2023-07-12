5d216978db3a8e2f4bf7db2c37077db5b87eed72
Abdelrahman Mohamed <obada.semary@gmail.com>
2023-07-13 00:38:59 +0300

    Add book trip to station service

--- VoltlinesBusStations/Network/Services/StationsService.swift
+++ VoltlinesBusStations/Network/Services/StationsService.swift
@@ -9,10 +9,18 @@ import Foundation
 
 protocol StationsServiceable {
     func getStations() async -> Result<[Station], RequestError>
+    func bookTrip(stationId: String, tripId: String) async -> Result<Trip, RequestError>
 }
 
 struct StationsService: HTTPClient, StationsServiceable {
     func getStations() async -> Result<[Station], RequestError> {
         return await sendRequest(endpoint: StationsEndpoint.stations, responseModel: [Station].self)
+    }
+
+    func bookTrip(stationId: String, tripId: String) async -> Result<Trip, RequestError> {
+        return await sendRequest(
+            endpoint: StationsEndpoint.bookTrip(stationId: stationId, tripId: tripId),
+            responseModel: Trip.self
+        )
     }
 }
