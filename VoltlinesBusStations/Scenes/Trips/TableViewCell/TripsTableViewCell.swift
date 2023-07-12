d179c6afe4383f466bdf1270e3d006339fa4c8b5
Abdelrahman Mohamed <obada.semary@gmail.com>
2023-07-12 03:28:37 +0300

    Add TripsTableViewCell

---
+++ VoltlinesBusStations/Scenes/Trips/TableViewCell/TripsTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TripsTableViewCell.swift
+//  VoltlinesBusStations
+//
+//  Created by Abdelrahman Mohamed on 11.07.2023.
+//
+
+import UIKit
+
+class TripsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
