18695d398629931f6d95309f362d030bcd9111eb
Abdelrahman Mohamed <obada.semary@gmail.com>
2023-07-14 16:49:13 +0300

    Add TripsVC+TripsPresenterDelegate

---
+++ VoltlinesBusStations/Scenes/Trips/TripsVC+TripsPresenterDelegate.swift
@@ -0,0 +1,39 @@
+//
+//  TripsVC+TripsPresenterDelegate.swift
+//  VoltlinesBusStations
+//
+//  Created by Abdelrahman Mohamed on 14.07.2023.
+//
+
+import Foundation
+
+extension TripsViewController: TripsPresenterDelegate {
+
+    func showIndicator() {
+        activityIndicator.startAnimating()
+    }
+
+    func hideIndicator() {
+        DispatchQueue.main.async {
+            self.activityIndicator.stopAnimating()
+            self.activityIndicator.hidesWhenStopped = true
+        }
+    }
+
+    func showsuccessBooking() {
+
+        DispatchQueue.main.async {
+            self.showAlert(alertType: .success)
+        }
+    }
+
+    func showError() {
+        DispatchQueue.main.async {
+            self.showAlert(alertType: .failure)
+        }
+    }
+
+    func presentStations(trips: [Trip]) {
+        updateSnapShot(trips: trips)
+    }
+}
