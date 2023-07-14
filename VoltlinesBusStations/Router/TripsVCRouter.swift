37daf87b2c3db98379f6a9454935499c25989d66
Abdelrahman Mohamed <obada.semary@gmail.com>
2023-07-14 16:53:00 +0300

    Add popTripsVC to tripsVCRouter

--- VoltlinesBusStations/Router/TripsVCRouter.swift
+++ VoltlinesBusStations/Router/TripsVCRouter.swift
@@ -12,9 +12,14 @@ class TripsVCRouter {
     class func createTripsVC(station: Station) -> UIViewController {
         
         let tripsVC = TripsViewController()
-        let presenter = TripsPresenter(delegate: tripsVC, station: station)
+        let router = TripsVCRouter()
+        let presenter = TripsPresenter(delegate: tripsVC, router: router, station: station)
         tripsVC.presenter = presenter
         return tripsVC
+    }
+
+    func popTripsVC(view: TripsViewController) {
+        view.navigationController?.popViewController(animated: true)
     }
 }
 
